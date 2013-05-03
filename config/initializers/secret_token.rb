4b3372a27c71b8abd79c3267112917f21ba82bfc
Andreas Hoffmann <furizaa@gmail.com>
2013-05-03 23:50:27 +0200

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Merkredo::Application.config.secret_token = '37a680eb1242ac02e473100116faaf6d7482536a00bda444d943728095a4ac067126c1fd99ff19f2535bd5aefbc1c4f7a1d2a7ac8db1edfc2287f005685b7f1d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Merkredo::Application.config.session_store :cookie_store, key: '_Merkredo_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Merkredo::Application.config.session_store :active_record_store
